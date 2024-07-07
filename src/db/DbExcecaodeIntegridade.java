9d36d41547a6c661aff0a3c7a86fecb626789dc6
Ronaldo Santos <ronaldo.tboupt@gmail.com>
2024-07-07 18:39:38 -0300

    Criando projeto e repositório no GitHub

---
+++ src/db/DbExcecaodeIntegridade.java
@@ -0,0 +1,13 @@
+package db;
+
+public class DbExcecaodeIntegridade extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DbExcecaodeIntegridade(String msg) {
+		super(msg);
+	}
+	
+	
+
+}
