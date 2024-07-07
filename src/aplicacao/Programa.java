580daafe98fcd015b60b6fc753e651723a7b77f1
Ronaldo Santos <ronaldo.tboupt@gmail.com>
2024-07-07 19:06:43 -0300

    Classe vendedores

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,13 +1,17 @@
 package aplicacao;
 
+import java.util.Date;
+
 import model.entindade.Departamento;
+import model.entindade.Vendedores;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
 		Departamento obj = new Departamento(1,"Livros");
-		System.out.println(obj);
+		Vendedores vendedores = new Vendedores(21,"Bob","bob@gmail.com",new Date(),3000.0,obj);
+		System.out.println(vendedores);
 
 	}
 
